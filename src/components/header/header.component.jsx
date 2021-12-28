262099356e4d3e14478b6cb3a415b41f9a1c492c
emmanuelokoye <emmanuelokoye@quickcheck.ng>
2021-12-28 14:53:53 +0100

    integrated firebase into application

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import { ReactComponent as Logo } from "../../assets/crown.svg";
+import { auth } from "../../../src/firebase/firebase.utils";
 
 import "./header.styles.scss";
 
-const Header = () => (
+const Header = ({ currentUser }) => (
   <div className="header">
     <Link to="/" className="logo-container">
       <Logo className="logo-img" />
@@ -18,6 +19,16 @@ const Header = () => (
       <Link className="option" to="/contact">
         CONTACT
       </Link>
+      {currentUser ? (
+        <div className="option" onClick={() => auth.signOut()}>
+          SIGN OUT
+        </div>
+      ) : (
+        <Link className="option" to="/signin">
+          SIGN IN
+        </Link>
+        
+      )}
     </div>
   </div>
 );
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAALnxjaW85gSMXGGMnxU04Vxb4INyg6bw",
+  authDomain: "kingsway-db.firebaseapp.com",
+  projectId: "kingsway-db",
+  storageBucket: "kingsway-db.appspot.com",
+  messagingSenderId: "154544506704",
+  appId: "1:154544506704:web:17bb414a24d55c8d0f4e55",
+  measurementId: "G-XJ4SCP2L4R",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+
+export const signInWithGoogle = () => signInWithPopup(auth, provider);
